d6563104e74550439393c713aee085a11f11e255
Dasha <daryaishchuk@gmail.com>
2022-12-20 20:17:22 +0300

    Недоделка

--- task27/Program.cs
+++ task27/Program.cs
@@ -4,16 +4,16 @@
 // 9012 -> 12
 
 Console.Write("Введите число: ");
-int number = int.Parse(Console.ReadLine());
-int GetSumNums(int sum);
+int num = int.Parse(Console.ReadLine()!);
+void GetSumNums(int number)
 
 {
     int sum = 0;
-    while( number > 10)
+    while(number > 0)
 {
-    number = number/10;
     sum += number%10;
+    number = number / 10;
 }
-    return sum;
-Console.WriteLine($"Сумма чисел = {GetSumNums(sum)}");
+Console.WriteLine($"Сумма чисел = {sum}");
 }
+GetSumNums(num);
