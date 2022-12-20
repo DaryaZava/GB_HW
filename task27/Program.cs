1d6708cd2c520458e15a960dc304c151e3fb3fcc
Dasha <daryaishchuk@gmail.com>
2022-12-20 19:04:30 +0300

    Недоделка

---
+++ task27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int GetSumNums(int sum);
+
+{
+    int sum = 0;
+    while( number > 10)
+{
+    number = number/10;
+    sum += number%10;
+}
+    return sum;
+Console.WriteLine($"Сумма чисел = {GetSumNums(sum)}");
+}
